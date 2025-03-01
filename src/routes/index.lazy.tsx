06fd51eff2acd587f19ced7af8178286729644fa
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-01 18:43:09 +0530

    feat: created join-create-chat.tsx form component with support of light and dark theme

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { toast } from "sonner";
+import JoinCreateChat from "../components/join-create-chat";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
@@ -7,16 +7,8 @@ export const Route = createLazyFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3 className="text-3xl font-bold">Welcome Home!</h3>
-      <button
-        onClick={() => {
-          toast.success("Hello from Loop!");
-        }}
-        className="bg-blue-500 text-white p-2 rounded mt-2 cursor-pointer"
-      >
-        Click me!
-      </button>
+    <div className="">
+      <JoinCreateChat />
     </div>
   );
 }
