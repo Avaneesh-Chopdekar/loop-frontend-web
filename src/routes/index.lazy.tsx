250d2473bde7663eba6a50ab62813bf8f75ba4a9
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-03-01 18:07:50 +0530

    style: configured tailwind css

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -7,7 +7,7 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3 className="text-3xl font-bold">Welcome Home!</h3>
     </div>
   );
 }
